b83d593c7636a51d7dba05b0868f6e1ca5886bba
Akash Singsarva <223701199+akashsarva07@users.noreply.github.com>
2026-03-13 20:52:13 +0530

    Modified RedisConfig class.

--- src/main/java/com/javawithakash/springbootredis/config/RedisConfig.java
+++ src/main/java/com/javawithakash/springbootredis/config/RedisConfig.java
@@ -10,9 +10,9 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
 
     @Bean
-    public RedisTemplate redisTemplate(RedisConnectionFactory factory) {
+    public RedisTemplate<String,Object> redisTemplate(RedisConnectionFactory factory) {
 
-        RedisTemplate redisTemplate = new RedisTemplate();
+        RedisTemplate<String,Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(factory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
